de0e73a199c1fb434e4369ed2fb045597c08a3ce
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-06-29 10:41:59 +0530

    admin completed

--- routes/admin.js
+++ routes/admin.js
@@ -6,53 +6,69 @@ const adminRouter = Router();
 adminRouter.post("/create", async (req, res) => {
   try {
     const { bookName, author, category } = req.body;
-     if(bookName){
-        return res.json({
-            msg: "Book details is present already"
-        })
+
+    const existingBook = await BookModel.findOne({ bookName });
+
+    if (existingBook) {
+      return res.json({
+        msg: "Book details is present already",
+      });
     }
-    await BookModel.create({
-      bookName,
-      author,
-      category,
-    });
 
     if (!bookName || !author || !category) {
       return res.status(404).json({
         msg: "Invalid credentials",
       });
     } else {
-      res.status(201).json({
-        msg: "Book added successfully",
+      await BookModel.create({
+        bookName,
+        author,
+        category,
       });
     }
+    res.status(201).json({
+      msg: "Book added successfully",
+    });
   } catch (error) {
     console.log(error);
   }
 });
 
-adminRouter.put('/book/:id', async(req, res) => {
-    try {
-        const id = req.params.id
-        const {bookName, author, category} = req.body
+adminRouter.put("/book/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { bookName, author, category } = req.body;
 
-        await BookModel.updateOne({
-            _id: id
-        },{
-            bookName,
-            author,
-            category
-        })
+    await BookModel.updateOne(
+      {
+        _id: id,
+      },
+      {
+        bookName,
+        author,
+        category,
+      },
+    );
 
-        res.json({
-            msg: "updated successfully"
-        })
-    } catch (error) {
-        console.log(error)
-         res.status(500).json({
-            msg: "Internal Server Error"
-        });
-    }
-})
+    res.json({
+      msg: "updated successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: "Internal Server Error",
+    });
+  }
+});
+
+adminRouter.delete("/book/:id", async (req, res) => {
+  const id = req.params.id;
+  await BookModel.deleteOne({
+    _id: id,
+  });
+  res.json({
+    msg: "Deleted successfully",
+  });
+});
 
 export default adminRouter;
